095ad29c6c19d765c1638f60c7a537467779a3dc
Sz4b1 <molnarszabolcs77285@gmail.com>
2024-10-08 12:42:44 +0200

    feat: :white_check_mark: Kesz feladat
    f1 es f2 feladatnak lett keszitve megoldas

--- f1/f1.js
+++ f1/f1.js
@@ -1,7 +1,16 @@
 
 const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
 async function f1 (lat, long, timeStr) {
-    //TODO
+    const URL = APIAdress + `latitude=${lat}&longitude=${long}` +
+    `&start_date=${timeStr}&end_date=${timeStr}&hourly=temperature_2m`;
+    const Fetch_data = await fetch(URL);
+    const Response = await Fetch_data.json();
+    const Daily_temperatures = Response.hourly.temperature_2m;
+    const Data_array = Daily_temperatures;
+    var AVG_temperature = 0;
+    Data_array.forEach(element => {
+        AVG_temperature+=element;   
+    });
+    return Math.floor(AVG_temperature/Data_array.length);
 }
-
 module.exports = f1;
